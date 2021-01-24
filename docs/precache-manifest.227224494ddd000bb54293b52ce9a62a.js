1959009e9b941b3bd8f3b8f5c575d21ea2dc8c44
Juan Villa <juanestebanvillamemdoza@gmail.com>
2021-01-24 11:22:05 -0400

    Folder public renamed to docs

---
+++ docs/precache-manifest.227224494ddd000bb54293b52ce9a62a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30e782be8ff2c228ef2da65a9aa304b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "379d351be9f39450a9ea",
+    "url": "/static/css/main.e4dc01c1.chunk.css"
+  },
+  {
+    "revision": "0ba099e73eb1fd4a4a28",
+    "url": "/static/js/2.fd12d035.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fd12d035.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "379d351be9f39450a9ea",
+    "url": "/static/js/main.c2bbb657.chunk.js"
+  },
+  {
+    "revision": "ede06adfc8d41a12f0e7",
+    "url": "/static/js/runtime-main.c607270d.js"
+  }
+]);
