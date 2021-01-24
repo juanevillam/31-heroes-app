063c2456f2d98a27aa13d93e8711c6ec842fbe4d
Juan Villa <juanestebanvillamemdoza@gmail.com>
2021-01-24 11:35:27 -0400

    File AppRouter.js updated

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { HashRouter as Router, Switch } from 'react-router-dom';
 
 import { AuthContext } from '../auth/AuthContext';
 import { PrivateRoute } from './PrivateRoute';
