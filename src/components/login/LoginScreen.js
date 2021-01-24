eea5933dcd8c7b1e42657224411ae83389e0ceb4
Juan Villa <juanestebanvillamemdoza@gmail.com>
2021-01-24 11:09:04 -0400

    Folder components/login/ added

---
+++ src/components/login/LoginScreen.js
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react';
+import { types } from '../../types/types';
+import { AuthContext } from '../../auth/AuthContext';
+
+export const LoginScreen = ( { history } ) => {
+
+    const { dispatch } = useContext( AuthContext );
+
+    const handleLogin = () => {
+
+        const lastPath = localStorage.getItem('lastPath') || '/';
+
+        dispatch({
+            type: types.login,
+            payload: {
+                name: 'Juan Villa'
+            }
+        });
+
+        history.replace( lastPath );
+        
+    };
+
+    return (
+        <div className="container mt-5">
+            <h1>Login, Welcome to the HeroesApp</h1>
+            <hr />
+
+            <button
+                className="btn btn-info"
+                onClick={ handleLogin }
+            >
+                Login
+            </button>
+
+        </div>
+    );
+
+};
